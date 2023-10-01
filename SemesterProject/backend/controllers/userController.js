3429aaa1f4e89d2723468b6179b4bfb7f0bfe351
malikayan827 <malikayan827@gmail.com>
2023-10-01 18:20:26 +0500

    jwt generation

--- SemesterProject/backend/controllers/userController.js
+++ SemesterProject/backend/controllers/userController.js
@@ -1,7 +1,9 @@
 const Product = require("../models/productModel");
+const sendToken = require("../utils/jwttoken");
 const ErrorHandler = require("../utils/errorhandler");
 const User=require('../models/userModel')
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
 //register a user
 exports.registerUser = catchAsyncErrors( async(req, res, next) => {
   
@@ -15,10 +17,26 @@ exports.registerUser = catchAsyncErrors( async(req, res, next) => {
             url:'profilepic'
         }
     })
-    res.status(201).json({
-        success:true,
-        user
-    })
+    sendToken(user,201,res);
 
 
 })
+//login user
+exports.loginUser = catchAsyncErrors( async(req, res, next) => {
+    const {email,password}=req.body;
+    if(!email || !password){
+        return next(new ErrorHandler('Please enter email and password',400));
+    }
+
+    const user=await User.findOne({email}).select('+password');
+    if(!user){
+        return next(new ErrorHandler('Invalid email or password',401));
+    }
+    //check if password is correct or not
+    const isPasswordMatched=await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler('Invalid email or password',401));
+    }
+    sendToken(user,200,res);
+}
+)
