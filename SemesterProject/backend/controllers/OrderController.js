7b30758d5c4adf9c6f6f2145830fc62c8927e9e6
malikayan827 <malikayan827@gmail.com>
2023-11-10 00:22:14 +0500

    all orders

--- SemesterProject/backend/controllers/OrderController.js
+++ SemesterProject/backend/controllers/OrderController.js
@@ -1,6 +1,6 @@
 const Order = require('..//models/orderModel')
 const Product = require('../models/productModel')
-const ErrorHandler = require('../utils/errorHandler')
+const ErrorHandler = require('..//utils/errorhandler')
 const catchAsyncErrors = require('..//middleware/catchAsyncErrors')
 exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     const {
@@ -28,3 +28,37 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
         order
     })
 })
+//get single order
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+    const order=await Order.findById(req.params.id).populate('user','name email')
+    if(!order){
+        return next(new ErrorHandler('No Order found with this ID',404))
+    }
+    res.status(200).json({
+        success:true,
+        order,
+    })
+})
+//get logged in user orders
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders=await Order.find({user:req.user.id})
+    res.status(200).json({
+        success:true,
+        orders,
+    })
+})
+//get all orders
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders=await Order.find()
+    let totalAmount=0
+    orders.forEach(order=>{
+        totalAmount+=order.totalPrice
+    })
+    res.status(200).json({
+        success:true,
+        totalAmount,
+        orders,
+    })
+}
+)
+//update order status
--- SemesterProject/backend/routes/OrderRoutes.js
+++ SemesterProject/backend/routes/OrderRoutes.js
@@ -1,6 +1,8 @@
 const  express=require("express")
 const { isAuthenticatedUsers,authorizeRoles } = require('../middleware/auth');
-const { newOrder } = require("../controllers/OrderController");
+const { newOrder, getSingleOrder, myOrders } = require("../controllers/OrderController");
 const router =express.Router()
 router.route("/order/new").post(isAuthenticatedUsers,newOrder)
+router.route("/order/:id").get(isAuthenticatedUsers,getSingleOrder)
+router.route("/orders/me").get(isAuthenticatedUsers,myOrders)
 module.exports=router;
